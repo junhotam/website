22330cc7f20ea3a67f024ec8d33827ca29925a64
Austin Tam <108040793+junhotam@users.noreply.github.com>
2023-06-08 21:21:18 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('scroll', function() {
   var footer = document.getElementById('footer');
   var scrollPosition = window.innerHeight + window.pageYOffset;
   var bodyHeight = document.body.offsetHeight;
-  var offset = -100; // Adjust this value as needed
+  var offset = -150; // Adjust this value as needed
 
   if (scrollPosition >= bodyHeight + offset) {
     footer.style.bottom = '0';
