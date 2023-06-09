1b035678700034e136a5381f6c17fb1a49792aa9
Austin Tam <108040793+junhotam@users.noreply.github.com>
2023-06-08 18:26:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+window.addEventListener('scroll', function() {
+  var footer = document.getElementById('footer');
+  var scrollPosition = window.innerHeight + window.pageYOffset;
+  var bodyHeight = document.body.offsetHeight;
+  if (scrollPosition >= bodyHeight) {
+    footer.style.bottom = '0';
+  } else {
+    footer.style.bottom = '-100px';
+  }
+});
